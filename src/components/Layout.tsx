928daf1dbc72d753dabe63cdb74ec45812b63c0b
Claudio Soto <stanle40@gmail.com>
2025-05-14 22:33:13 -0400

    Deploy application to Netlify

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -351,7 +351,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                 <div className="hidden md:block">
                   <div className="relative" ref={searchRef}>
                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <Search className="h-5 w-5 text-gray-600" />
+                      <Search className="h-5 w-5 text-gray-400" />
                     </div>
                     <input
                       type="text"
