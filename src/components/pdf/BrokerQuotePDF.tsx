f1df0d7e57616afa54574f1f4be1136a7038b124
Claudio Soto <stanle40@gmail.com>
2025-05-22 20:20:02 -0400

    Deploy application to Netlify

--- src/components/pdf/BrokerQuotePDF.tsx
+++ src/components/pdf/BrokerQuotePDF.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Document, Page, Text, View, StyleSheet, Font } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet, Font, Image } from '@react-pdf/renderer';
 
 // Helper function for formatting
 const formatCurrency = (amount: number | null): string => {
