001d2c1e670ef1ed9486060bc8c95738c2eae042
Claudio Soto <stanle40@gmail.com>
2025-05-26 09:14:09 -0400

    Deploy application to Netlify

--- src/pages/public/BrokerQuotePage.tsx
+++ src/pages/public/BrokerQuotePage.tsx
@@ -388,7 +388,7 @@ const BrokerQuotePage: React.FC = () => {
     const montoDescuentoDisponible = precioOriginal - precioConComision;
 
     const descuentoDisponibleFrac = montoDescuentoDisponible / precioOriginal;
-    return Math.max(0, Math.round(descuentoDisponibleFrac ));
+    return Math.max(0, Math.round(descuentoDisponibleFrac ) / 100);
   };
   
   // Handle unit selection
